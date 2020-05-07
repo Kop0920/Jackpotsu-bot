84d05ae8d12b6109f5156768ad42e531c8622c2b
Kop0920 <63380174+Kop0920@users.noreply.github.com>
2020-05-07 14:11:13 +0900

    Update injex.py

--- injex.py
+++ injex.py
@@ -92,9 +92,10 @@ async def 투표(ctx):
     
  
 
-if message.content.startswith("역할부여김유섭):
+if message.content.startswith("역할부여김유섭"):
     role = ""
-    rolename = discord.utils.get(client.get_all_members(), id=rolename[1])
+    rolename = message.content.split(" ")
+    member = discord.utils.get(cilent.get_all_members(), id=rolename[1])
     for i in message.server.roles:
         if i.name == rolename[2]:
             role = i
