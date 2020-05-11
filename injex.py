b5879674fa994c5e8998417bb420bd7ff543906d
Kop0920 <63380174+Kop0920@users.noreply.github.com>
2020-05-12 07:48:24 +0900

    Update injex.py

--- injex.py
+++ injex.py
@@ -92,6 +92,21 @@ async def 투표(ctx):
     
 
 
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!ping'):
+        await client.send_message(message.channel, 'pong')
+
+
 
 
 
