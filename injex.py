546fa4bef10fcf728c4a619a187967671ad18db6
Kop0920 <63380174+Kop0920@users.noreply.github.com>
2020-05-22 18:12:42 +0900

    Update injex.py

--- injex.py
+++ injex.py
@@ -91,7 +91,7 @@ async def 투표(ctx):
         await choose.add_reaction("👍")
     
 
-@bot.command()
+@client.command()
 async def 역할부여(message):
     author = message.guild.get_member(int(message.content[6:24]))
     name = message.content[25:] 
